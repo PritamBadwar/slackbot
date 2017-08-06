80fcbaadfc1598257dbb2accaf5a775834d1dc11
PritamBadwar <pritam.badwar@gmail.com>
2017-08-06 17:31:36 +0530

    Update azuretest.py

--- azuretest.py
+++ azuretest.py
@@ -1,4 +1,18 @@
-a = 2
-b = 3
+from selenium import webdriver
+import requests, bs4, BeautifulSoup
 
-print(a+b)
+driver = webdriver.Firefox()
+driver.get("https://www.google.com/search?q=9393033723")
+
+results = driver.find_elements_by_css_selector('div.g')
+
+link = results[0].find_element_by_tag_name("a")
+href = link.get_attribute("href")
+print href
+
+##driver.get(href)
+##
+##results2 = driver.find_elements_by_class_name("mreinfwpr")
+
+r = requests.get(href)
+print r.content
